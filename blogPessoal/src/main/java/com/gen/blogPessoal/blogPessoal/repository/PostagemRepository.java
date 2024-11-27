a302bcff6f3fe83cfebb5782b2bd884202b3dbd3
Kevin Schuab <kevinschuab@gmail.com>
2024-11-27 18:18:39 -0300

    first commit

---
+++ blogPessoal/src/main/java/com/gen/blogPessoal/blogPessoal/repository/PostagemRepository.java
@@ -0,0 +1,9 @@
+package com.gen.blogPessoal.blogPessoal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gen.blogPessoal.blogPessoal.model.Postagem;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+
+}
