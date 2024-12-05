b41139d306abd67a40bde58c6b706ae1526522a0
Kevin Schuab <kevinschuab@gmail.com>
2024-12-05 16:47:11 -0300

    feat: implementando segurança no projeto por meio de Spring Security

---
+++ blogPessoal/blogPessoal/src/main/java/com/gen/blogPessoal/blogPessoal/model/Usuario.java
@@ -0,0 +1,49 @@
+package com.gen.blogPessoal.blogPessoal.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message = "O atributo nome é obrigatório")
+	private String nome;
+
+	@NotNull(message = "O atributo usuario é obrigatório")
+	@Email(message = "O atributo usuario deve ser um email válido")
+	private String usuario;
+
+	@NotNull(message = "O atributo senha é obrigatório")
+	@Size(min = 8, message = "A senha deve conter no mínimo 8 caracteres")
+	private String senha;
+
+	@Size(max = 5000, message = "O link da foto não pode ser maio do que 5000 caracteres")
+	private String foto;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+
+}
