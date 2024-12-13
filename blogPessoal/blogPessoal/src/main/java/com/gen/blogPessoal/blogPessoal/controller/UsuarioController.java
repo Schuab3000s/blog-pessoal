1b89a7f1a6c417d232223de37d7fba70cef12d3d
Kevin Schuab <kevinschuab@gmail.com>
2024-12-13 17:49:45 -0300

    Implementando a documentação do Swagger

--- blogPessoal/blogPessoal/src/main/java/com/gen/blogPessoal/blogPessoal/controller/UsuarioController.java
+++ blogPessoal/blogPessoal/src/main/java/com/gen/blogPessoal/blogPessoal/controller/UsuarioController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.gen.blogPessoal.blogPessoal.model.Postagem;
 import com.gen.blogPessoal.blogPessoal.model.Usuario;
 import com.gen.blogPessoal.blogPessoal.model.UsuarioLogin;
 import com.gen.blogPessoal.blogPessoal.repository.UsuarioRepository;
